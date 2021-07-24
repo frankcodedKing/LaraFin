6ef1adf246db6bcc4635bc1d1c26532cb362410c
harrison ozioko <harrigold.18@gmail.com>
2021-07-24 13:58:57 +0100

    just pages worked on by nanoc

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1,11 +1,104 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Companydetail;
+use App\Models\Faq;
 
 use Illuminate\Http\Request;
 
 class adminController extends Controller
 {
+
+    // model-wheredataisstored
+    // rowid-tabletostore
+    // dataArray-datatobestored-colum,value
+    public function savedata($mymodel, $rowid, $dataArray)
+    {
+        $model = $mymodel;
+        if ($rowid == null) {
+            # code...
+            $rowselected = $model::where("id",1)->first();
+            if ($rowselected == null) {
+                # code...
+                $newrow = new $model;
+                foreach ($dataArray as $key => $value) {
+                    $newrow->$key=$value;
+                }
+                if ($newrow->save()) {
+                    # code...
+                    return true;
+                                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                # code...
+                foreach ($dataArray as $key => $value) {
+                    $rowselected->$key=$value;
+                }
+                if ( $rowselected->save() ) {
+                    # code...
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        elseif (gettype($rowid)=="integer") {
+            # code...
+            $rowselected = $model::where("id",$rowid)->first();
+            if ($rowselected == null) {
+                # code...
+                return false;
+            } else {
+                # code...
+                foreach ($dataArray as $key => $value) {
+                    $rowselected->$key=$value;
+                }
+                if ($rowselected->save()) {
+                    # code...
+                    return true;
+                } else {
+                    # code...
+                    return false;
+                }
+            }
+        }
+        else {
+            # code...
+            $newentry =  new $model;
+            foreach ($dataArray as $key => $value) {
+                $newentry->$key=$value;
+            }
+            if ($newentry->save()) {
+                # code...
+                return true;
+            } else {
+                # code...
+                return false;
+            }
+        }
+    }
+
+    //deletefn
+    public function deleteRow()
+    {
+        $row = $model::where("id", $rowid)->first();
+        if ($row == null) {
+            return"row to delete not found";
+        } else {
+            if ($row->delete) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+
     //
     public function adminindex()
     {
@@ -14,7 +107,11 @@ class adminController extends Controller
 
     public function pages()
     {
-        return view("admin.pages");
+        $aboutCompany = Companydetail::where("id", 1)->first();
+        $faqs = Faq::all();
+        $data = ["companyDetail"=>$aboutCompany, "faqs"=>$faqs];
+
+        return view("admin.pages",$data);
     }
 
     public function users()
@@ -55,55 +152,36 @@ class adminController extends Controller
     }
 
 
-    // model-wheredataisstored
-    // rowid-tabletostore
-    // dataArray-datatobestored-colum,value
-    public function savedata($model, $rowid, $dataArray)
-    {
-        //selects
-        if ($rowid == null) {
-            $row = $model::all;
-            foreach ($dataArray as $key => $value) {
-                $row->$key=$value;
-            }
-            if ($row->save()) {
-                return true;
-            } else {
-                return false;
-            }
-            if (gettype($rowid)=="integer") {
-                $row=$model::where("id", $rowid)->first();
 
-                if ($row!==null) {
-                    foreach ($dataArray as $key=> $value) {
-                        $row->$key=$value;
-                    }
-                    if ($row->save()) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return "row not found";
-                }
-            } else {
-                return false;
-            }
-        }
+
+
+    public function savecompanydetails (Request $req){
+
+$companyName =  $req->companyname;
+$runningDays =  $req->runningdays;
+$companyEmail =  $req->companyemail;
+$companyLocation =  $req->companylocation;
+$companyContact =  $req->companycontact;
+
+$saveArray = [
+    'companyName'=>$companyName,
+    'runningDays' =>$runningDays,
+    'companyEmail' =>$companyEmail,
+     'companyLocation' => $companyLocation,
+      'companyPhone'=> $companyContact
+    ];
+
+$result = $this->savedata(Companydetail::class, null , $saveArray);
+
+    if ($result) {
+        # code...
+        return redirect()->route("pages")->with("success", "Update was succesful");
+    } else {
+        # code...
+        return redirect()->route("pages")->with("error", "Failed to update try again!");
     }
 
-    //deletefn
-    public function deleteRow()
-    {
-        $row = $model::where("id", $rowid)->first();
-        if ($row == null) {
-            return"row to delete not found";
-        } else {
-            if ($row->delete) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+
+
     }
 }
